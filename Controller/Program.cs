16b1977d78ad5d7e0a3e0f7cccf9a5c0fb449081
Mads Hammer <madshammer@hotmail.com>
2024-12-20 13:58:08 +0100

    Program cs øvelse

--- Controller/Program.cs
+++ Controller/Program.cs
@@ -1,4 +1,32 @@
 
 
+using HillerødSejlKlub.Models;
+using HillerødSejlKlub.Services;
+
+BoatRepository boatRepository = new BoatRepository();
+
+Boat boat = new Boat("Test", "Tesla", HillerødSejlKlub.Data.BoatType.Sejlbåd, 34, 1994, "324242");
+
+boatRepository.AddBoat(boat);
+
+List<Boat> boatList = new List<Boat>();   
+
+Dictionary<int, Boat> boats = boatRepository.GetAll();
+
+foreach (var entry in boats)
+{
+
+    int id = entry.Key;
+    Console.WriteLine(entry);
+}
+
+boatRepository.UpdateBoatByID(3, "test2", "testmodel2", HillerødSejlKlub.Data.BoatType.TERA, 14);
+
+foreach (var entry in boats)
+{
+
+    int id = entry.Key;
+    Console.WriteLine(entry.Value);
+}
 
 
